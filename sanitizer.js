0926bcc34e7261730a2dd7127135a241e4dbdc6a
Maurice Schoenmakers <m@x-x.io>
2016-01-17 17:01:38 +0100

    initial

---
+++ sanitizer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Utilizes the sanitizer to strip out dirty stuff, which needs to get inserted into the DOM.
+ */
+
+var sanitizer = require('sanitizer');
+
+module.exports = sanitizer;
